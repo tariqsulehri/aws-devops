96ae4d30989c8c307845383c10dce71244204120
tariqsulehri <tariq_sulehri@hotmail.com>
2025-11-12 18:14:04 +0500

    updated vpc root main.tf

--- terraform/src/main.tf
+++ terraform/src/main.tf
@@ -21,10 +21,10 @@
 # ROOT MAIN - module wiring
 ##############################################
 
-vpc_cidr             = "10.0.0.0/16"
-availability_zones   = ["eu-north-1a", "eu-north-1b"]
-public_subnet_cidrs  = ["10.0.1.0/24", "10.0.3.0/24"]
-private_subnet_cidrs = ["10.0.2.0/24", "10.0.4.0/24"]
+# vpc_cidr             = "10.0.0.0/16"
+# availability_zones   = ["eu-north-1a", "eu-north-1b"]
+# public_subnet_cidrs  = ["10.0.1.0/24", "10.0.3.0/24"]
+# private_subnet_cidrs = ["10.0.2.0/24", "10.0.4.0/24"]
 
 
 module "vpc" {
